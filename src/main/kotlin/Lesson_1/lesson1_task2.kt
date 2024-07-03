a1db9d736dc2ce20eb095e29576a07cf0f285fd8
EfremovPG <pavelefremovmail@mail.ru>
2024-07-03 14:53:51 +0300

    KS-1-2 check

---
+++ src/main/kotlin/Lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package Lesson_1
+
+fun main() {
+    val CompliteOrders:Int = 75
+    val ThankPurchase:String = "Спасибо за покупку!"
+    println("Вы совершили $CompliteOrders заказов! $ThankPurchase")
+
+    var NumberOfWorker:Int = 2000
+//    println("Колличество сотрудников $NumberOfWorker")
+
+        NumberOfWorker = 1999
+    println("Колличество сотрудников $NumberOfWorker")
+
+}
