6d154183ebdc94c770d9c7cd85f5665e1dbab2ec
EfremovPG <pavelefremovmail@mail.ru>
2024-07-03 14:55:57 +0300

    KS-1-4 check

---
+++ src/main/kotlin/Lesson_1/lesson1_task4.kt
@@ -0,0 +1,19 @@
+package Lesson_1
+
+import com.sun.tools.attach.AgentLoadException
+
+fun main() {
+    val Lenght:Long = 40868600000
+    val Age:Int = 27
+    val PartOfDay:Double = 0.075
+    val Seconds:Int = 6480
+    val PartOfYear: Double = 2.0547945205479453E-4
+    val Apogee:Int = 327000
+println("""$Lenght
+    |$Age
+    |$PartOfDay
+    |$Seconds
+    |$PartOfYear
+    |$Apogee
+""".trimMargin())
+}
