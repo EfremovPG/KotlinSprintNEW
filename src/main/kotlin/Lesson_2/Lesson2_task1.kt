8b3a67885e664edf841f28ca8dab2e2a752d39d0
EfremovPG <pavelefremovmail@mail.ru>
2024-07-03 15:38:22 +0300

    KS-2-1 Check

---
+++ src/main/kotlin/Lesson_2/Lesson2_task1.kt
@@ -0,0 +1,12 @@
+package Lesson_2
+
+fun main(){
+    val AllStudent:Float = 4f   //Достаточно один раз указать Float или нужно у каждой
+                                //переменной проставлять (ведь они все теперь Float)
+    val Student1 = 3
+    val Student2 = 4
+    val Student3 = 3
+    val Student4 = 5
+
+    println((Student1 + Student2 + Student3 + Student4) / AllStudent)
+}
