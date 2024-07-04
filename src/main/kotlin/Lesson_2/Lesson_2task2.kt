bc3a654910fe402fe3ef0c29aa4c9792f85e5b99
EfremovPG <pavelefremovmail@mail.ru>
2024-07-04 10:38:05 +0300

    KS-2-2 Check добавил строку между решением и выводом на экран

--- src/main/kotlin/Lesson_2/Lesson_2task2.kt
+++ src/main/kotlin/Lesson_2/Lesson_2task2.kt
@@ -10,6 +10,7 @@ fun main(){
     val SalaryStudent = Student * PayStudent
     val SalaryAll = SalaryRegular + SalaryStudent
     val AverageSalaryAll = SalaryAll / (RegularWorker + Student)
+
     println("""Расходы на выплату зарплаты постоянных сотрудников $SalaryRegular руб.
         |Общие расходы по ЗП после прихода стажеров $SalaryAll руб.зарплата
         |Средняя ЗП одного сотрудника после устройства стажеров $AverageSalaryAll руб.
