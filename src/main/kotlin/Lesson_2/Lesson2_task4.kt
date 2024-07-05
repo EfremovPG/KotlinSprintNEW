b95d9ef2d796a91efe751802673c1f4d49bb3134
EfremovPG <pavelefremovmail@mail.ru>
2024-07-05 16:27:41 +0300

    KS-2-4 Check исправлены замечания

--- src/main/kotlin/Lesson_2/Lesson2_task4.kt
+++ src/main/kotlin/Lesson_2/Lesson2_task4.kt
@@ -1,13 +1,14 @@
 package Lesson_2
 
 fun main(){
-    val CrystalOre = 7
-    val IronOre = 11
-    val Buff = 20
-    val CystalBuff = CrystalOre * Buff / 100
-    val OreBuff = IronOre * Buff / 100
+    val allPercent = 100
+    val crystalOre = 7
+    val ironOre = 11
+    val buff = 20
+    val cystalBuff = crystalOre * buff / allPercent
+    val oreBuff = ironOre * buff / allPercent
 
-    println("""Увеличение добычи Кристаллов на $CystalBuff
-        |Увеличение добычи Железа на $OreBuff""".trimMargin())
+    println("""Увеличение добычи Кристаллов на $cystalBuff
+        |Увеличение добычи Железа на $oreBuff""".trimMargin())
 
 }
