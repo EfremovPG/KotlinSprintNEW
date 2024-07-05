8f137b3bb8547de7273b3380014593a1d69bf56c
EfremovPG <pavelefremovmail@mail.ru>
2024-07-05 16:03:48 +0300

    KS-1-5 Check исправлены замечания

--- src/main/kotlin/Lesson_1/lesson1_task5.kt
+++ src/main/kotlin/Lesson_1/lesson1_task5.kt
@@ -1,12 +1,12 @@
 package Lesson_1
 
 fun main() {
-
+    val secInMinute = 60
     val timeInSeconds = 6480
-    val timeInMimute = timeInSeconds / 60
-    val hour = timeInMimute / 60
-    val minute = timeInMimute % 60
-    val second = timeInSeconds % 60
+    val timeInMimute = timeInSeconds / secInMinute
+    val hour = timeInMimute / secInMinute
+    val minute = timeInMimute % secInMinute
+    val second = timeInSeconds % secInMinute
 
     println(String.format("Время в космосе: %02d:%02d:%02d", hour, minute, second))
 
