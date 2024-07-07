c3ecdbabad0af91e1fa44ddd3061178ea5e2dedf
EfremovPG <pavelefremovmail@mail.ru>
2024-07-07 15:53:16 +0300

    KS-3-1 Check

---
+++ src/main/kotlin/Lesson_3/Lesson3_task1.kt
@@ -0,0 +1,13 @@
+package Lesson_3
+
+fun main() {
+    val username = "Пользователь"
+    var greeting = "Добрый день, "
+
+    println("$greeting $username")
+
+    greeting = "Добрый вечер, "
+
+    println("$greeting $username")
+
+}
