6e942df2a14ffe139453ea9ce63c2754f5e11f9d
EfremovPG <pavelefremovmail@mail.ru>
2024-07-08 16:57:11 +0300

    KS-2-5 Check

---
+++ src/main/kotlin/Lesson_2/Lesson2_task5.kt
@@ -0,0 +1,15 @@
+package Lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+    val deposit = 70000
+    val rate = 16.7
+    val time = 20
+
+    val result = deposit * (1 + rate / 100).pow(time)
+
+    println("Размер вклада через $time лет:$result")
+
+
+}
