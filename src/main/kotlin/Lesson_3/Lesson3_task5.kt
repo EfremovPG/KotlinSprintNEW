78217218dd93597db2417ba0f5952869b61c72fd
EfremovPG <pavelefremovmail@mail.ru>
2024-07-09 21:46:11 +0300

    KS-3-5 Check изменил решение на split

--- src/main/kotlin/Lesson_3/Lesson3_task5.kt
+++ src/main/kotlin/Lesson_3/Lesson3_task5.kt
@@ -3,15 +3,15 @@ package Lesson_3
 fun main() {
 
     val turn = "D2-D4;0"
-    val from = "${turn[0]}${turn[1]}"
-    val to = "${turn[3]}${turn[4]}"
-    val turnNumber = "${turn[6]}"
+    val data = turn.split("-", ";")
+    val from = data[0]
+    val to = data[1]
+    val turnNumber = data[2]
 
     println(
         """Ход № $turnNumber,
         |Переместили фигуру с клетки $from на клетку $to
     """.trimMargin()
     )
-
 }
 
