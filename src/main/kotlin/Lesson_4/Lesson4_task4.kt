5520e1764dc18338b46135ad5db3711991cf4463
EfremovPG <pavelefremovmail@mail.ru>
2024-07-11 17:27:58 +0300

    KS-4-4 убрал привязку к частям тела, оставил только чет/нечет

--- src/main/kotlin/Lesson_4/Lesson4_task4.kt
+++ src/main/kotlin/Lesson_4/Lesson4_task4.kt
@@ -2,19 +2,16 @@ package Lesson_4
 
 fun main() {
     val dayOfTraining = 5
-
-    val handTraining = (dayOfTraining % 2) != 0
-    val absTtaning = (dayOfTraining % 2) != 0
-    val legTraning = (dayOfTraining % 2) == 0
-    val backTraning = (dayOfTraining % 2) == 0
+    val isEven = (dayOfTraining % 2) != 0
+    val isNotEven = (dayOfTraining % 2) == 0
 
     println(
         """Сегодня делаем упражнения-
-    |Упражнения для рук:$handTraining
-    |Упражнения для пресса:$absTtaning
+    |Упражнения для рук:$isEven
+    |Упражнения для пресса:$isEven
     |
-    |Упражнения для ног:$legTraning
-    |Упражнения для спины:$backTraning
+    |Упражнения для ног:$isNotEven
+    |Упражнения для спины:$isNotEven
 """.trimMargin()
     )
 }
